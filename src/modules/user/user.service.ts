f4ae3628768e872fc008d60a7890a717b809757b
Hack-Light <onohsomtochukwubasil@gmail.com>
2023-09-07 20:43:10 +0100

    initial user setup

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private userRepository: UserRepository) {}
+
+  async createUser(params: User) {
+    const user = await this.userRepository.createUser({ data: params });
+    return user;
+  }
+
+  async getUser(id: number) {
+    const user = await this.userRepository.getUser({ where: { id } });
+    return user;
+  }
+
+  async updateUser(id: number, data: User) {
+    const user = await this.userRepository.updateUser({ where: { id }, data });
+    return user;
+  }
+}
