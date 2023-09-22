4bd066b7274822f64fe1879f99fa16cf2c0a1555
cipher-tech <nickchibuikem@gmail.com>
2023-09-22 02:32:02 +0100

    added cors support

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters';
 import rawBodyMiddleware from './utils/rawBody.middleware';
-import { ConfigService } from "@nestjs/config";
-import { Logger } from "@nestjs/common";
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { ValidationPipe } from './pipes';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger("NestApplication");
+  const logger = new Logger('NestApplication');
 
-  app.enableCors({origin: ["*"]});
-  
+  app.enableCors({
+    origin: ['http://localhost:3000', '*'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(rawBodyMiddleware());
-  app.setGlobalPrefix("/api");
+  app.setGlobalPrefix('/api');
   const configService = app.get(ConfigService);
-  const port = configService.get("PORT");
+  const port = configService.get('PORT');
 
   await app.listen(port);
   logger.log(`Application is running on: ${await app.getUrl()}`);
