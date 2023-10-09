1d7718c998250cac2c08fd28ae1f50e2108bc562
Hack-Light <onohsomtochukwubasil@gmail.com>
2023-10-09 07:12:42 +0100

    initial draft of maintenece

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,28 +6,32 @@ import { AuthModule } from './modules/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JWT_SECRET } from './base';
 import { UserModule } from './modules/user/user.module';
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { envVarsSchema } from "./helpers/env.validator";
+import { ConfigModule } from '@nestjs/config';
+import { envVarsSchema } from './helpers/env.validator';
 import { SuiteModule } from './modules/suite/suite.module';
 import { BusinessModule } from './modules/business/business.module';
 import { EmailModule } from './modules/email/email.module';
 
-
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    cache: true,
-    validationSchema: envVarsSchema,
-  }),
-  {
-    ...JwtModule.register({
-      secret: JWT_SECRET,
-      signOptions: { expiresIn: "24h" },
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: envVarsSchema,
     }),
-    global: true,
-  },
-    PrismaModule, 
-    AuthModule, UserModule, SuiteModule, BusinessModule, EmailModule
+    {
+      ...JwtModule.register({
+        secret: JWT_SECRET,
+        signOptions: { expiresIn: '24h' },
+      }),
+      global: true,
+    },
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    SuiteModule,
+    BusinessModule,
+    EmailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
