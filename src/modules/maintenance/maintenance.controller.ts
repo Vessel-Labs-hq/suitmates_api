9cd4dd23af36dcada1fcda17d6838b0a3749e89e
Hack-Light <onohsomtochukwubasil@gmail.com>
2023-10-23 02:04:41 +0100

    Total requests and pending requests

--- src/modules/maintenance/maintenance.controller.ts
+++ src/modules/maintenance/maintenance.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UploadedFiles } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UploadedFiles,
+} from '@nestjs/common';
 import { MaintenanceService } from './maintenance.service';
 import { CreateMaintenanceDto } from './dto/create-maintenance.dto';
 import { UpdateMaintenanceDto } from './dto/update-maintenance.dto';
@@ -11,25 +21,38 @@ import { IUser, User } from 'src/decorators';
 import { HttpResponse } from 'src/utils/http-response.utils';
 import { AwsS3Service } from 'src/aws/aws-s3.service';
 
-
-@UseGuards(AuthGuard,RolesGuard)
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('maintenance')
 export class MaintenanceController {
-  constructor(private readonly maintenanceService: MaintenanceService, private readonly awsS3Service: AwsS3Service) {}
+  constructor(
+    private readonly maintenanceService: MaintenanceService,
+    private readonly awsS3Service: AwsS3Service,
+  ) {}
 
   @Post()
   @ValidatedImages('images')
   @Roles(Role.Tenant)
-  async create(@Body() createMaintenanceDto: CreateMaintenanceDto,@UploadedFiles() images: Express.Multer.File[],@User() user: IUser) {
-    
-    const result = await this.maintenanceService.createMaintenanceRequest(createMaintenanceDto,user);
+  async create(
+    @Body() createMaintenanceDto: CreateMaintenanceDto,
+    @UploadedFiles() images: Express.Multer.File[],
+    @User() user: IUser,
+  ) {
+    const result = await this.maintenanceService.createMaintenanceRequest(
+      createMaintenanceDto,
+      user,
+    );
     if (Array.isArray(images) && images.length) {
-     
       for (const image of images) {
-        const uploadedImage = await this.awsS3Service.uploadFile(image.originalname, image.buffer);
-        await this.maintenanceService.createMaintenanceImage(result,uploadedImage);
+        const uploadedImage = await this.awsS3Service.uploadFile(
+          image.originalname,
+          image.buffer,
+        );
+        await this.maintenanceService.createMaintenanceImage(
+          result,
+          uploadedImage,
+        );
       }
-    } 
+    }
     return HttpResponse.success({
       data: result,
       message: 'Maintenance request created successfully',
@@ -41,13 +64,21 @@ export class MaintenanceController {
     return this.maintenanceService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.maintenanceService.findOne(+id);
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.maintenanceService.findOne(+id);
+  // }
+
+  @Get(':userId')
+  getOwnerMaintenanceBoard(@Param('userId') userId: string) {
+    return this.maintenanceService.getMaintenanceRequestsByUser(+userId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMaintenanceDto: UpdateMaintenanceDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateMaintenanceDto: UpdateMaintenanceDto,
+  ) {
     return this.maintenanceService.update(+id, updateMaintenanceDto);
   }
 
--- src/modules/maintenance/maintenance.service.ts
+++ src/modules/maintenance/maintenance.service.ts
@@ -1,37 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateMaintenanceDto } from './dto/create-maintenance.dto';
 import { UpdateMaintenanceDto } from './dto/update-maintenance.dto';
 import { UserService } from '../user/user.service';
 import { PrismaService } from 'src/database/prisma.service';
-import { connect } from 'net';
+import { ErrorHelper } from 'src/utils';
 
 @Injectable()
 export class MaintenanceService {
-  constructor(private userService: UserService,private prisma: PrismaService){}
-  async createMaintenanceRequest(createMaintenanceDto: CreateMaintenanceDto,user: any) {
-    
+  constructor(
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
+  async createMaintenanceRequest(
+    createMaintenanceDto: CreateMaintenanceDto,
+    user: any,
+  ) {
     const tenant = await this.userService.findOne(user.id);
     const maintenanceRequest = await this.prisma.maintenanceRequest.create({
-      data:{
-        user: {connect: {id: tenant.id}},
-        suite: {connect: {id: tenant.suite?.id}},
+      data: {
+        user: { connect: { id: tenant.id } },
+        suite: { connect: { id: tenant?.suite?.id } },
         priority: createMaintenanceDto.priority,
         description: createMaintenanceDto.description,
         repair_date: null,
         repair_time: null,
-      }
-    })
-    return maintenanceRequest
+      },
+    });
+    return maintenanceRequest;
   }
 
-  async createMaintenanceImage(maintenanceRequest: any,url:string){
-
+  async createMaintenanceImage(maintenanceRequest: any, url: string) {
     const maintenanceImage = await this.prisma.maintenanceImages.create({
-      data:{
+      data: {
         url: url,
-        maintenance_request: {connect: {id: maintenanceRequest.id}}
+        maintenance_request: { connect: { id: maintenanceRequest.id } },
+      },
+    });
+  }
+
+  async getMaintenanceRequestsByUser(userId: number): Promise<any> {
+    try {
+      // step 0: Validate User
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: { id: userId },
+      });
+
+      if (!user) ErrorHelper.BadRequestException('User not found');
+
+      // Step 1: Retrieve all suites by a user
+      const userSuites = await this.prisma.space.findMany({
+        where: { owner_id: userId },
+      });
+
+      const maintenanceRequests: any[] = [];
+      let totalMaintenanceRequests = 0;
+      let pendingMaintenanceRequests = 0;
+
+      // Step 2: Iterate through the retrieved suites
+      for (const suite of userSuites) {
+        // Step 3: For each suite, retrieve its associated maintenance requests
+        const suiteMaintenanceRequests =
+          await this.prisma.maintenanceRequest.findMany({
+            where: { suite_id: suite.id },
+          });
+
+        // Step 4: Filter maintenance requests to find the pending ones
+        const pendingRequests = suiteMaintenanceRequests.filter(
+          (request) => request.status === 'PENDING',
+        );
+
+        // Add the suite's maintenance requests to the result array
+        maintenanceRequests.push(...suiteMaintenanceRequests);
+
+        // Update counts
+        totalMaintenanceRequests += suiteMaintenanceRequests.length;
+        pendingMaintenanceRequests += pendingRequests.length;
       }
-    })
+
+      // Step 5: Return the result
+      return {
+        maintenanceRequests,
+        totalMaintenanceRequests,
+        pendingMaintenanceRequests,
+      };
+    } catch (error) {
+      console.log(error);
+      ErrorHelper.InternalServerErrorException(error?.message);
+    }
   }
 
   findAll() {
@@ -42,7 +98,7 @@ export class MaintenanceService {
     return `This action returns a #${id} maintenance`;
   }
 
-  update(id: number, updateMaintenanceDto: UpdateMaintenanceDto) {
+  update(id: number, _updateMaintenanceDto: UpdateMaintenanceDto) {
     return `This action updates a #${id} maintenance`;
   }
 
