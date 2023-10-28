fb4c5264e54d5b386dbbd9efbfd164fa39bc1ca3
Kid-jnr <normansinto3@gmail.com>
2023-10-28 12:56:53 +0100

    fixed invite tenant issue

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body,UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RegisterTenantDto } from './dto/register-tenant.dto';
 import { VerifyTokenDto } from './dto/verify-token.dto';
 import { HttpResponse } from 'src/utils/http-response.utils';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 import { IUser, User } from 'src/decorators';
 
 @Controller('auth')
@@ -21,9 +25,10 @@ export class AuthController {
     return this.authService.register(registerUserDto);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Owner)
   @Post('register-tenant')
   async registerTenant(@Body() registerTenantDto: RegisterTenantDto,@User() user: IUser) {
-    
     await this.authService.registerTenant(registerTenantDto,user.id);
     return HttpResponse.success({
       data: '',
