5921399fe8421f47927156a923b6a705f9de1b05
Kid-jnr <normansinto3@gmail.com>
2023-12-13 14:45:17 +0100

    added email

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -6,7 +6,7 @@ export class EmailService {
     constructor(private mailerService: MailerService) {}
 
     async sendUserWelcome(email: any, password: string) {
-        const confirmation_url = `https://beta.tenant.mysuitemates.com/auth/login?email=${email}&password=${password}`;
+        const confirmation_url = ` https://beta.tenant.mysuitemates.com/auth/verify?email=${email}&password=${password}`;
     
         await this.mailerService.sendMail({
           to: email,
